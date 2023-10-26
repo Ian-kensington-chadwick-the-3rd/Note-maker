91f700cc8c18a76a2b664dde8b9ec30284e7e9f5
Ian Sills <iansills04@gmail.com>
2023-10-25 20:24:18 -0500

    initialized Note-maker

---
+++ public/assets/js/pseudocode.js
@@ -0,0 +1,18 @@
+// import express module to our sever
+// import require('./db/db.json')
+
+// create app variable pointing to new express object 
+// app.use(*middleware*)
+
+// middleware that we need -json, urlencoded, express.static('public')
+// get/delete/post request
+
+// GET request route that serves up our index.html, to send notes from db.json route: '/'
+// GET request to send notes.html, route: '/notes' 
+// GET request to fetch our api to push to modify with parsed req.body data and push post to db.json with fs.writeFile, route: '/api/notes' return res.json
+// post request to fetch our api, route: '/api/notes'
+// delete request to delete specific note, route: '/api/notes/: notes_id'
+// app.listen
+
+
+// go to activity 18
